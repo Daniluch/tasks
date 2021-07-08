3db6bec87b17da261265351014e99f46aa039083
hakerman007 <75410986+hakerman007@users.noreply.github.com>
2021-07-08 20:55:47 +0300

    Add files via upload

---
+++ JS Tasks/script.js
@@ -0,0 +1,109 @@
+'use strict';
+
+/* jshint esversion: 6 */
+
+//START
+
+const tasks = {
+
+    palmTree: (number) => {
+
+        let numberOfp = '';
+
+        for (let i = 0; i < number; i++) {
+
+            numberOfp += 'p';
+
+        }
+
+        return isNaN(number) ? false : numberOfp;
+
+    },
+
+    autoReplace: (array, changeWord, text) => {
+
+        let replaceFn = () => {
+
+            array.map(element => {
+
+                while (text.includes(element)) {
+            
+                    text = text.replace(element, changeWord);
+        
+                }
+
+            });
+    
+            return text;   
+
+        };
+
+        return (typeof array === 'object' && typeof changeWord === 'string' && typeof text === 'string') ? replaceFn() : false;
+
+    },
+
+    genitive: (n, word) => {
+
+        let genitiveFn = () => {
+
+            let words = {
+
+                singular: word,
+
+                many1: `${word}а`,
+
+                many2: `${word}ов`
+
+            };
+
+            return (n.toString().slice(-1) === "1" && n.toString().slice(-2) !== "11") ?  `${n} ${words.singular}` : 
+
+            (+n.toString().slice(-1) < 5 && n.toString().slice(-2) !== "11" && n.toString().slice(-1) !== '0' && n.toString().slice(-2).slice(0,1) !== "1") ? `${n} ${words.many1}` :
+
+            (+n.toString().slice(-1) > 4 || +n.toString().slice(-2) > 9 && +n.toString().slice(-2) < 22 || n.toString().slice(-1) === '0') ? `${n} ${words.many2}` : false;
+
+        };
+
+        return (typeof n === 'number' && typeof word === 'string') ? genitiveFn() : false;
+
+    },
+
+    lorem: (n) => {
+
+        let text = "sdjfhjbvddfdfdsshjdfsdfhdghdfgfdgdfgfdgdfgdfgdfgjfhjbvddfdfdsshjdfsdfhdghdfgfdgdfgfdgdjfhjbvddfdfdsshjdfsdfhdghdfgfdgdfgfdgdjfhjbvddfdfdsshjdfsdfhdghdfgfdgdfgfdgdjfhjbvddfdfdsshjdfsdfhdghdfgfdgdfgfdgdjfhjbvddfdfdsshjdfsdfhdghdfgfdgdfgfdgdjfhjbvddfdfdsshjdfsdfhdghdfgfdgdfgfdgdjfhjbvddfdfdsshjdfsdfhdghdfgfdgdfgfdgdjfhjbvddfdfdsshjdfsdfhdghdfgfdgdfgfdgdjfhjbvddfdfdsshjdfsdfhdghdfgfdgdfgfdgdjfhjbvddfdfdsshjdfsdfhdghdfgfdgdfgfdgd";
+
+        let slicedText = '';
+
+        let count = 0;
+
+        for (let i of text) {
+
+            slicedText += i;
+
+            count++;
+
+            if (count > n) break;
+ 
+        } 
+
+        return slicedText;
+
+    },
+
+    myReplace: (text, sliceWord, changeWord) => {
+
+        let newText = text;
+
+        while (newText.includes(sliceWord)) {
+
+            newText = newText.replace(sliceWord, changeWord);
+
+        }
+
+        return newText;
+
+    }
+
+};
+
+console.log(tasks.genitive(2,"курс"));
